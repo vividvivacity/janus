657ef33a9fa1ff5c5ffb7dc4b6e4170979e9c522
vividvivacity <yakaxdiamond@gmail.com>
2020-11-08 15:06:03 -0800

    update message blocks and formatting

--- onboarding_tutorial.py
+++ onboarding_tutorial.py
@@ -1,5 +1,5 @@
 class OnboardingTutorial:
-    """Constructs the onboarding message and stores the state of which tasks were completed."""
+    """Constructs the onboarding message."""
 
     WELCOME_BLOCK = {
         "type": "section",
@@ -7,10 +7,71 @@ class OnboardingTutorial:
             "type": "mrkdwn",
             "text": (
                 "Greetings, mortal. I am Janus, the all-knowing observer bot. :eye-in-speech-bubble:\n\n"
+                "To get started, type \"janus help\" for a list of my commands.\n\n"
                 "*Learn more about me below:*"
             ),
         },
     }
+
+    MISSION_BLOCK = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "*Background:*\n"
+                "With so many places having moved to online environments, Slack has become a great way "
+                "for peers and colleagues to communicate, especially for asking questions and receiving "
+                "help. Unfortunately, not everyone is always online to help other users. This can be "
+                "especially troublesome for questions that get asked over and over, but receive no answers "
+                "for a while.\n\n"
+                "*Mission:*\n"
+                "Janus aims to cut down on wait time for answers by determining if the question has been "
+                "asked before in the workspace, see if there are replies to the question, then deliver that "
+                "information to the user straight away. Through these means, Janus ensures that those who "
+                "have questions which have already been answered before but do not know due to lack of time "
+                "and communication will be helped."
+            )
+        }
+    }
+
+    DEV_BLOCK = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "*Progress in Development:*\n"
+                "Currently, basic functionalities of what Janus aims to do have been implemented, though "
+                "the methods of implementation plan to be improved on in the future. Right now, Janus "
+                "utilizes the searching feature of the Slack Web API to find matching questions and a "
+                "naive method of determining whether a message is a question. In the future, Janus plans "
+                "to:\n\n"
+                "• determine matches between questions through semantic similarity "
+                "(meaning of a sentence rather than the structure of a sentence).\n"
+                "• improve the method of choosing the best matching question. One idea is to score "
+                "questions, choose a group of matching questions if they pass a certain threshold, "
+                "and choose the question with the most replies.\n"
+                "• implement a feedback system to keep track of how useful Janus currently is to users "
+                "and make improvements to the bot."
+            )
+        }
+    }
+
+    INFO_BLOCK = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "*Why am I named Janus?*\n"
+                "Janus is the name of the Roman god of beginnings, ends, doorways, and duality. Janus "
+                "has two faces: one that looks into the past, and one that looks into the future. In "
+                "this context, Janus the bot looks into past questions and uses them to help answer "
+                "similar ones in the future. Also the name Janus is cool.\n\n"
+                "You can read more about Janus the Roman god *<https://en.wikipedia.org/wiki/Janus|"
+                "here>*."
+            )
+        }
+    }
+
     DIVIDER_BLOCK = {"type": "divider"}
 
     def __init__(self, channel):
@@ -30,47 +91,10 @@ class OnboardingTutorial:
             "blocks": [
                 self.WELCOME_BLOCK,
                 self.DIVIDER_BLOCK,
-                *self._get_reaction_block(),
+                self.MISSION_BLOCK,
                 self.DIVIDER_BLOCK,
-                *self._get_pin_block(),
+                self.DEV_BLOCK,
+                self.DIVIDER_BLOCK,
+                self.INFO_BLOCK
             ],
         }
-
-    def _get_reaction_block(self):
-        task_checkmark = self._get_checkmark(self.reaction_task_completed)
-        text = (
-            f"{task_checkmark} *Add an emoji reaction to this message* :thinking_face:\n"
-            "You can quickly respond to any message on Slack with an emoji reaction."
-            "Reactions can be used for any purpose: voting, checking off to-do items, showing excitement."
-        )
-        information = (
-            ":information_source: *<https://get.slack.help/hc/en-us/articles/206870317-Emoji-reactions|"
-            "Learn How to Use Emoji Reactions>*"
-        )
-        return self._get_task_block(text, information)
-
-    def _get_pin_block(self):
-        task_checkmark = self._get_checkmark(self.pin_task_completed)
-        text = (
-            f"{task_checkmark} *Pin this message* :round_pushpin:\n"
-            "Important messages and files can be pinned to the details pane in any channel or"
-            " direct message, including group messages, for easy reference."
-        )
-        information = (
-            ":information_source: *<https://get.slack.help/hc/en-us/articles/205239997-Pinning-messages-and-files"
-            "|Learn How to Pin a Message>*"
-        )
-        return self._get_task_block(text, information)
-
-    @staticmethod
-    def _get_checkmark(task_completed: bool) -> str:
-        if task_completed:
-            return ":white_check_mark:"
-        return ":white_large_square:"
-
-    @staticmethod
-    def _get_task_block(text, information):
-        return [
-            {"type": "section", "text": {"type": "mrkdwn", "text": text}},
-            {"type": "context", "elements": [{"type": "mrkdwn", "text": information}]},
-        ]
